172af72052749780e581aa004041c0e51b12c4d9
Cyril Khan <ckhan@tocco.ch>
2018-12-28 14:54:42 +0100

    feat(Header): add styled components and header

--- bob/src/components/header.js
+++ bob/src/components/header.js
@@ -1,34 +1,21 @@
-import { Link } from 'gatsby'
+// import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
+import styled from 'styled-components'
 
-const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
+const StyledHeader = styled.div`
+  color: white;
+  margin: 0 auto;
+  height: 3rem; 
+  text-align: center;
+  background-color: black;
+  padding: 3rem 0;
+`
+
+const Header = () => (
+  <StyledHeader>
+    Test
+  </StyledHeader>
 )
 
 Header.propTypes = {
--- bob/src/components/layout.js
+++ bob/src/components/layout.js
@@ -22,9 +22,10 @@ const Layout = ({ children }) => (
         <div
           style={{
             margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
+            display: `block`,
+            backgroundColor: 'black',
+            padding: '0 2rem',
+            height: '100vh'
           }}
         >
           {children}
