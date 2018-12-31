ca6e0dc65af8d702603f0c21de0d956c72f76aa9
Cyril Khan <ckhan@tocco.ch>
2018-12-31 10:25:19 +0100

    finish part four

--- tutorial-part-four/src/pages/index.js
+++ tutorial-part-four/src/pages/index.js
@@ -1,12 +1,11 @@
 import React from "react"
-import { graphql } from 'gatsby'
+import { Link, graphql } from 'gatsby'
 import { css } from '@emotion/core'
 import { rhythm } from '../utils/typography'
 import Layout from '../components/layout'
 
 export default ({ data }) => (
   <Layout>
-    {console.log(data)}
     <div>
       <h1
         css={css`
@@ -19,21 +18,29 @@ export default ({ data }) => (
       <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
       {data.allMarkdownRemark.edges.map(({ node }) => (
         <div key={node.id}>
-          <h3
+          <Link
+            to={node.fields.slug}
             css={css`
+              text-decoration: none;
+              color: inherit;
+             `}
+          >
+            <h3
+              css={css`
               margin-bottom: ${rhythm(1/4)};
             `}
-          >
-            - {node.frontmatter.title}{" "}
-            <span
-              css={css`
+            >
+              - {node.frontmatter.title}{" "}
+              <span
+                css={css`
                 color: #bbb;
               `}
-            >
+              >
               - {node.frontmatter.date}
             </span>
-          </h3>
-          <p>{node.excerpt}</p>
+            </h3>
+            <p>{node.excerpt}</p>
+          </Link>
         </div>
       ))}
     </div>
@@ -56,6 +63,9 @@ export const query = graphql`
           frontmatter {
             title
             date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
           }
           excerpt
         }
