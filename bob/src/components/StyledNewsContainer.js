32cb47538303f3bc3be325c2d60b7423f4446e17
Cyril Khan <ckhan@tocco.ch>
2019-01-03 11:32:13 +0100

    feat: add NewsContainer

--- bob/src/components/StyledNewsContainer.js
+++ bob/src/components/StyledNewsContainer.js
@@ -4,20 +4,20 @@ const StyledNewsContainer = styled.div`
   width: 60%;
   margin: 0 auto;
   padding: 5rem 5rem 3rem 5rem;
-  text-align: left;
+  text-align: center;
   border: 1px solid white;
   border-radius: 5px;
   
   h2 {
     line-height: 1.5;
-    text-align: center;
   }
    @media (max-width: 650px) {
    border: none;
    width: 90%;
+   text-align: left;
+
     h2 {
       font-size: 22px;
-      text-align: left;
     }
     p {
       font-size: 14px;
